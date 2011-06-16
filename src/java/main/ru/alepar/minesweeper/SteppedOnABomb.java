e00cdd0166a761f2f75afcb8031d46113f5f22c0
alepar <alepar@gmail.com>
2011-06-16 20:26:38 +0400

    typo fix

--- src/java/main/ru/alepar/minesweeper/SteppedOnABomb.java
+++ src/java/main/ru/alepar/minesweeper/SteppedOnABomb.java
@@ -3,7 +3,7 @@ package ru.alepar.minesweeper;
 public class SteppedOnABomb extends Exception {
 
     public SteppedOnABomb(Point p) {
-        super(String.format("stepped on a bomdb at (%d, %d)", p.x, p.y));
+        super(String.format("stepped on a bomb at (%d, %d)", p.x, p.y));
     }
 
 }
